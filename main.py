66c4ae87a5263104e13bcfefd7b34c1d698be051
unknown <Egor Dimov>
2021-03-14 18:44:37 +0300

    sdf

---
+++ main.py
@@ -0,0 +1,73 @@
+import pygame
+
+window = pygame.display.set_mode((800,600))
+pygame.display.set_caption("niggers 3s")
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    pygame.display.update()
